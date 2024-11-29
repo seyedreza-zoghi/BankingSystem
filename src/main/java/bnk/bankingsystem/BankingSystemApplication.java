fa604caa31a2246f126a0b8a69efbee50e957832
seyedreza.zoghi <seyedreza.zoghi@gmail.com>
2024-11-29 17:45:30 +0330

    add value in to run

--- src/main/java/bnk/bankingsystem/BankingSystemApplication.java
+++ src/main/java/bnk/bankingsystem/BankingSystemApplication.java
@@ -25,21 +25,22 @@ public class BankingSystemApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        bankAccount.createAccount("Ali", new BigDecimal("1000.00"));
+        BankAccount ali = bankAccount.createAccount("Ali", new BigDecimal("1000.00"));
+        BankAccount reza = bankAccount.createAccount("reza", new BigDecimal("0"));
 
         // واریز وجه
         BigDecimal input1 = new BigDecimal("500.00");
-        bankAccount.deposit("ACC-12345", input1);
-        transactionLogger.onTransaction("ACC-12345", "Deposit", input1);
+        bankAccount.deposit(ali.getAccountNumber(), input1);
+        transactionLogger.onTransaction(ali.getAccountNumber(), "Deposit", input1);
 
         // برداشت وجه
         BigDecimal input2 = new BigDecimal("200.00");
-        bankAccount.withdraw("ACC-12345", input2);
-        transactionLogger.onTransaction("ACC-12345", "Withdraw", input2);
+        bankAccount.withdraw(ali.getAccountNumber(), input2);
+        transactionLogger.onTransaction(ali.getAccountNumber(), "Withdraw", input2);
 
         // انتقال وجه
         BigDecimal input3 = new BigDecimal("100.00");
-        bankAccount.transfer("ACC-12345", "ACC-67890", input3);
-        transactionLogger.onTransaction("ACC-12345", "Transfer", input3);
+        bankAccount.transfer(ali.getAccountNumber(), reza.getAccountNumber(), input3);
+        transactionLogger.onTransaction(ali.getAccountNumber(), reza.getAccountNumber(), input3);
     }
 }
